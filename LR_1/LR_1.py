5ec61bf40eff7c3edcd579e3aea316582fa116f1
Milen <148059423+MilenNastya@users.noreply.github.com>
2024-10-04 11:27:05 +0300

    Добавлена первая лабораторная работа с отчетом

--- LR_1/LR_1.py
+++ LR_1/LR_1.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 
 #Задание 2
 # img1 = cv2.imread('pic1.png',cv2.IMREAD_GRAYSCALE)
@@ -25,7 +25,7 @@ import cv2
 #     cv2.imshow('mov',frame1)
 #     if cv2.waitKey(1) & 0XFF == 27:
 #         break
-
+#
 # cap1=cv2.VideoCapture('my_video.mp4',cv2.CAP_ANY)
 # while True:
 #     ret, frame = cap1.read()
@@ -60,7 +60,7 @@ import cv2
 # cv2.destroyAllWindows()
 
 #Задание 5
-# чтение изображения
+# # чтение изображения
 # img = cv2.imread('pic1.jpg')
 # # окна для отображения изображений
 # cv2.namedWindow('Original Image', cv2.WINDOW_AUTOSIZE)
@@ -73,62 +73,88 @@ import cv2
 # cv2.destroyAllWindows()
 
 #Задание 6
+#
 # cap = cv2.VideoCapture(0)
-# frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#
+# # Определяем центр кадра
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#
 # while True:
-#     ret, frame = cap.read()
-#     if not(ret):
+#     ret, img = cap.read()
+#     if not ret:
 #         break
-#     flipped_frame = cv2.flip(frame, 1)
-#     sp1 = (int(frame_width/2)-15, int(frame_height/2)-60)
-#     ep1 = (int(frame_width/2)+15, int(frame_height/2)+60)
-#     sp2 = (int(frame_width/2)-60, int(frame_height/2)-15)
-#     ep2 = (int(frame_width/2)+60, int(frame_height/2)+15)
-#     roi1 = flipped_frame[sp2[1]:ep2[1], sp2[0]:ep2[0]]
-#     blurred_roi1 = cv2.GaussianBlur(roi1, (25, 25), 0)
-#     flipped_frame[sp2[1]:ep2[1], sp2[0]:ep2[0]] = blurred_roi1
-#     cv2.rectangle(flipped_frame, sp1, ep1, (0, 0, 255), 2)
-#     cv2.rectangle(flipped_frame, sp2, ep2, (0, 0, 255), 2)
-#     cv2.imshow('mp4', flipped_frame)
-#     if cv2.waitKey(1) & 0XFF == 27:
+#     mirrored_img = cv2.flip(img, 1)
+#
+#     # Горизонтальный прямоугольник
+#     start_point_horizontal = (width // 2 - 100, height // 2 - 20)
+#     end_point_horizontal = (width // 2 + 100, height // 2 + 20)
+#
+#     # Верхний вертикальный прямоугольник
+#     start_point_vertical_top = (width // 2 - 20, height // 2 - 100)
+#     end_point_vertical_top = (width // 2 + 20, height // 2 - 20)
+#
+#     # Нижний вертикальный прямоугольник
+#     start_point_vertical_bottom = (width // 2 - 20, height // 2 + 20)
+#     end_point_vertical_bottom = (width // 2 + 20, height // 2 + 100)
+#
+#     # Размытие горизонтального региона
+#     region_of_interest = mirrored_img[start_point_horizontal[1]:end_point_horizontal[1],
+#                          start_point_horizontal[0]:end_point_horizontal[0]]
+#     blurred_region = cv2.GaussianBlur(region_of_interest, (25, 25), 0)
+#     mirrored_img[start_point_horizontal[1]:end_point_horizontal[1],
+#     start_point_horizontal[0]:end_point_horizontal[0]] = blurred_region
+#
+#     overlay = mirrored_img.copy()
+#
+#     # Отрисовка прямоугольников
+#     cv2.rectangle(overlay, start_point_horizontal, end_point_horizontal, (0, 0, 255), 3)
+#     cv2.rectangle(overlay, start_point_vertical_top, end_point_vertical_top, (0, 0, 255), 3)
+#     cv2.rectangle(overlay, start_point_vertical_bottom, end_point_vertical_bottom, (0, 0, 255), 3)
+#
+#     alpha = 0.9
+#     mirrored_img = cv2.addWeighted(overlay, alpha, mirrored_img, 1 - alpha, 0)
+#
+#     cv2.imshow('mp4', mirrored_img)
+#     if cv2.waitKey(1) & 0xFF == 27:
 #         break
+#
 # cap.release()
 # cv2.destroyAllWindows()
 
 #Задание 7
-def readIPWriteTOFile():
-    video = cv2.VideoCapture(0)
-    ok, img = video.read()
-    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video_writer = cv2.VideoWriter("output_2.mp4", fourcc, 25, (w, h))
-    while (True):
-        ok, img = video.read()
-        cv2.imshow('Webcam video', img)
-        video_writer.write(img)
-        # выход при нажатии клавиши 'esc'
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-            video.release()
-            cv2.destroyAllWindows()
-
-readIPWriteTOFile()
+# def readIPWriteTOFile():
+#     video = cv2.VideoCapture(0)
+#     ok, img = video.read()
+#     w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+#     h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#     video_writer = cv2.VideoWriter("output_2.mp4", fourcc, 25, (w, h))
+#     while (True):
+#         ok, img = video.read()
+#         cv2.imshow('Webcam video', img)
+#         video_writer.write(img)
+#         # выход при нажатии клавиши 'esc'
+#         if cv2.waitKey(1) & 0xFF == 27:
+#             break
+#             video.release()
+#             cv2.destroyAllWindows()
+#
+# readIPWriteTOFile()
 
 #Задание 8
 # cap=cv2.VideoCapture(0)
-# frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 # while True:
 #     ret, frame = cap.read()
 #     if not(ret):
 #         break
-#     sp1 = (int(frame_width/2)-15, int(frame_height/2)-60)
-#     ep1 = (int(frame_width/2)+15, int(frame_height/2)+60)
-#     sp2 = (int(frame_width/2)-60, int(frame_height/2)-15)
-#     ep2 = (int(frame_width/2)+60, int(frame_height/2)+15)
-#     blue,green,red=frame[int(frame_width/2)][int(frame_height/2)]
+#     sp1 = (int(width/2)-15, int(height/2)-60)
+#     ep1 = (int(width/2)+15, int(height/2)+60)
+#     sp2 = (int(width/2)-60, int(height/2)-15)
+#     ep2 = (int(width/2)+60, int(height/2)+15)
+#     blue,green,red=frame[int(width/2)][int(height/2)]
 #     color=(blue,green,red)
 #     max_val = max(color)
 #     max_index = color.index(max_val)
@@ -141,43 +167,7 @@ readIPWriteTOFile()
 #         break
 # cv2.destroyAllWindows()
 
-# #Задание 9
-# cap=cv2.VideoCapture("rtsp://192.168.35.68:8080/h264_pcm.sdp")
-# while True:
-#     ret, frame = cap.read()
-#     if not(ret):
-#         break
-#     cv2.imshow('mp4',frame)
-#     if cv2.waitKey(1) & 0XFF == 27:
-#         break
 
-# url = "http://192.168.1.114:8080/h264_pcm.sdp"
-#
-# # Создайте VideoCapture объект с указанным URL
-# cap = cv2.VideoCapture(url)
-#
-# # Проверьте, был ли поток успешно открыт
-# if not cap.isOpened():
-#     print("Не удалось подключиться к камере. Проверьте URL и соединение.")
-#     exit()
-#
-# while True:
-#     ret, frame = cap.read()
-#
-#     if not ret:
-#         print("Не удается получить кадр. Попробуйте проверить соединение.")
-#         break
-#
-#     # Отображение кадра
-#     cv2.imshow('Видео с камеры телефона', frame)
-#
-#     # Условие для выхода из цикла по нажатию клавиши 'ESC'
-#     if cv2.waitKey(1) & 0xFF == 27:
-#         break
-#
-# # Освобождаем ресурсы
-# cap.release()
-# cv2.destroyAllWindows()
 
 
 
