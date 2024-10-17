c58b74fda63b3c9696a1051d66bb580cb0aaf506
Milen <148059423+MilenNastya@users.noreply.github.com>
2024-10-18 00:05:02 +0300

    Изменение 2 лабораторной работы

--- LR_2/LR_2.py
+++ LR_2/LR_2.py
@@ -10,108 +10,97 @@ import numpy as np
 #
 #     cv2.imshow('HSV', hsv)
 #
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#     if cv2.waitKey(1) & 0xFF == 27:
 #         break
 #
 # cap.release()
 # cv2.destroyAllWindows()
 
-#     # Задание 2: Применить фильтрацию изображения для выделения красного цвета
+#     # Задание 2: Применить фильтрацию изображения для выделения фиолетового цвета
 #     # Определяем диапазоны для красного цвета
 # cap = cv2.VideoCapture(0)
-#
 # while True:
-#     ret, frame = cap.read()
-#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#
-#     mask = cv2.inRange(hsv, (140,50,120), (179,255,255))
-#     onlyRed_frame = cv2.bitwise_and(frame, frame, mask = mask)
-#
-#     cv2.imshow('RED OBJ', onlyRed_frame)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#     ret, frame= cap.read()
+#     hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#     violet_mask = cv2.inRange(hsv_image, (130, 50, 120), (160, 255, 255))
+#     highlighted_violet = cv2.bitwise_and(frame, frame, mask=violet_mask)
+#     cv2.imshow('VIOLET OBJECTS', highlighted_violet)
+#     if cv2.waitKey(1) & 0xFF == 27:
 #         break
 #
+# # Освобождаем ресурсы
 # cap.release()
 # cv2.destroyAllWindows()
-#
-#     # Задание 3: Морфологические преобразования
 
-# cap = cv2.VideoCapture(0)
 #
+# #     # Задание 3: Морфологические преобразования
+# cap = cv2.VideoCapture(0)
 # while True:
 #     ret, frame = cap.read()
 #     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#
-#     mask = cv2.inRange(hsv, (140,50,120), (179,255,255))
-#     #mask = cv2.inRange(hsv, lower_red, upper_red)
+#     # определение диапазона красного цвета в HSV
+#     lower_red = np.array([0, 0, 100])  # минимальные значения оттенка, насыщенности и значения(яркости)
+#     upper_red = np.array([100, 100, 255])  # максимальные значения оттенка, насыщенности и значения(яркости)
+#     # Маска - бинарное изображение, где пиксели, соответствующие заданному диапазону цвета, имеют значение 255 (белый), а остальные пиксели имеют значение 0 (черный).
+#     mask = cv2.inRange(hsv, lower_red, upper_red)
+#     # применение маски на изображение
+#     res = cv2.bitwise_and(frame, frame, mask=mask)
+#     # структурирующий элемент(определяет размер и форму области)
+#     kernel = np.ones((5, 5), np.uint8)
+#     # применение операции открытия - позволяет удалить шумы и мелкие объекты на изображении(удаление нежелательных пикселей или деталей)
+#     opening = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
+#     # применение операции закрытия - позволяет заполнить маленькие пробелы и разрывы в объектах на изображении
+#     closing = cv2.morphologyEx(res, cv2.MORPH_CLOSE, kernel)
+#     cv2.imshow('Opening', opening)
+#     cv2.imshow('Closing', closing)
+#     if cv2.waitKey(1) & 0xFF == 27:
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
+
+# 4- 5 задание
+# cap = cv2.VideoCapture(0)
+# previous_cx, previous_cy = None, None
+# alpha = 0.6  # коэффициент сглаживания
+# while True:
+#     ret, frame = cap.read()
+#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#     lower_purple = np.array([130, 50, 50])  # минимальные значения оттенка, насыщенности и значения (яркости)
+#     upper_purple = np.array([160, 255, 255]) # максимальные значения оттенка, насыщенности и значения (яркости)
+#     mask = cv2.inRange(hsv, lower_purple, upper_purple)
 #
 #     kernel = np.ones((5, 5), np.uint8)
+#     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 #
-#     image_opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-#     image_closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-#     cv2.imshow("Open", image_opening)
-#     cv2.imshow("Close", image_closing)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# cap.release()
-# cv2.destroyAllWindows()
-#
-# def erode(image, kernel):
-#     m, n = image.shape
-#     km, kn = kernel.shape
-#     hkm = km // 2
-#     hkn = kn // 2
-#     eroded = np.copy(image)
-#
-#     for i in range(hkm, m - hkm):
-#         for j in range(hkn, n - hkn):
-#             eroded[i, j] = np.min(
-#                 image[i - hkm:i + hkm + 1, j - hkn:j + hkn + 1][kernel == 1])
-#
-#     return eroded
-#
-# def dilate(image, kernel):
-#     m, n = image.shape
-#     km, kn = kernel.shape
-#     hkm = km // 2
-#     hkn = kn // 2
-#     dilated = np.copy(image)
-#
-#     for i in range(hkm, m - hkm):
-#         for j in range(hkn, n - hkn):
-#             dilated[i, j] = np.max(
-#                 image[i - hkm:i + hkm + 1, j - hkn:j + hkn + 1][kernel == 1])
-#
-#     return dilated
-#
-# #     # Задание 4 и 5: Нахождение моментов и площади объекта
-# cap = cv2.VideoCapture(0)
-#
-# while True:
-#     ret, frame = cap.read()
-#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#
-#     mask = cv2.inRange(hsv, (140,50,120), (179,255,255))
-#     #mask = cv2.inRange(hsv, lower_red, upper_red)
-#
-#     moments = cv2.moments(mask)
+#     blurred = cv2.GaussianBlur(mask, (5, 5), 0)
+#     moments = cv2.moments(blurred)
 #     area = moments['m00']
 #
-#     if area > 0:
+#     if area > 500:  # минимальная площадь для отсечения шумов
+#         # ширина и высота прямоугольника равны квадратному корню из площади объекта
 #         width = height = int(np.sqrt(area))
+#         # вычисление координат центра объекта на изображении с использованием момент первого порядка
 #         c_x = int(moments["m10"] / moments["m00"])
 #         c_y = int(moments["m01"] / moments["m00"])
+#
+#         # Сглаживание движения прямоугольника
+#         if previous_cx is not None and previous_cy is not None:
+#             c_x = int(alpha * previous_cx + (1 - alpha) * c_x)
+#             c_y = int(alpha * previous_cy + (1 - alpha) * c_y)
+#
+#         previous_cx, previous_cy = c_x, c_y
+#         color = (0, 0, 0) # черный цвет
+#         thickness = 2 # толщина
 #         cv2.rectangle(frame,
-#             (c_x - (width // 12), c_y - (height // 12)),
-#             (c_x + (width // 12), c_y + (height // 12)),
-#             (0, 0, 0), 2)
+#             (c_x - (width // 20), c_y - (height // 20)),
+#             (c_x + (width // 20), c_y + (height // 20)),
+#             color, thickness)
 #
-#     cv2.imshow('Rectanle_frame', frame)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#     cv2.imshow('HSV_frame', hsv)
+#     cv2.imshow('Result_frame', frame)
+#     if cv2.waitKey(1) & 0xFF == 27:
 #         break
-#
 # cap.release()
 # cv2.destroyAllWindows()
+
+
